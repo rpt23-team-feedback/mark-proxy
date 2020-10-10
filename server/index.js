0de7e9a184c5b1f6a66198c406d2dc911c3b6f37
Mark <mark.scrudder@gmail.com>
2020-10-10 11:28:37 -0700

    Proxy routing set for tiersIncluded. Working.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
+const proxy = require('express-http-proxy');
 
 const PORT = 3103;
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../dist')));
 
-
-
 app.listen(PORT, () => {
   console.log('listening at port ', PORT);
 });
+
+app.use('/tiersIncluded', proxy('localhost:3101'));
 
 module.exports = app;
